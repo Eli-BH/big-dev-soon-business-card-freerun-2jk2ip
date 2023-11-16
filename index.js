2305184b83c5360db4461112860e6cebd9d73c2b
Eli Brown-Henderson <Eli.i.henderson95@gmail.com>
2023-11-16 16:29:07 -0500

    Attempt: 1

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-/* This is an example JavaScript file, feel free to remove/edit it anytime */
-console.log(
-  "%cProject by BigDevSoon",
-  'font-size: 40px; font-weight: bold; color: #8A2BE2; font-family: "Comic Sans MS", cursive, sans-serif;'
-);
-console.log("Check out more projects at https://bigdevsoon.me");
+window.onload = function () {
+  document.querySelector(".user-image").addEventListener("mouseenter", () => {
+    const shapes = document.getElementsByClassName("shape");
+
+    for (let i = 0; i < shapes.length; i++) {
+      const shape = shapes[i];
+      shape.style.opacity = "1";
+    }
+  });
+
+  document.querySelector(".user-image").addEventListener("mouseout", () => {
+    const shapes = document.getElementsByClassName("shape");
+
+    for (let i = 0; i < shapes.length; i++) {
+      const shape = shapes[i];
+      shape.style.opacity = "0";
+    }
+  });
+};
